3dfe891917ffadf7be766a7f26f653c53a61cc58
ahmedraza <raza2393@gmail.com>
2016-05-04 11:10:47 +0530

    MainActivity Changes- Button Added activity_main.xml- Changes in buttons etc

--- app/src/main/java/com/android/ahmedraza/laughcounter/MainActivity.java
+++ app/src/main/java/com/android/ahmedraza/laughcounter/MainActivity.java
@@ -1,13 +1,46 @@
 package com.android.ahmedraza.laughcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.dec);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+               if (x > 0){
+                   x = x--;
+               }
+            }
+        });
+
+
+        final Button button2 = (Button) findViewById(R.id.inc);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (x <= 0){
+                    x = x++;
+                }
+            }
+        });
     }
+
+    String newXvalue = Integer.toString(x);
+
+    TextView err = (TextView)findViewById(R.id.quantity_text_view);
+    err.setText("Hello");
+
+
 }
